efec0e62f63839bbb1b0664d6b710256fe20d982
Drew Miley <drewmiley92@gmail.com>
2019-01-13 02:17:38 +0000

    Adds simple api check and postman collection

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
-console.log('Server running');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+const router = express.Router();
+router.use((req, res, next) => {
+    console.log('Request made - logging from middleware');
+    next();
+});
+router.get('/', (req, res) => {
+    res.json({ message: 'Server running' });
+});
+app.use('/api', router);
+app.listen(8080);
