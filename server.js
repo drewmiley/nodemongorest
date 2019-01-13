03f55d9714f5167a6718c5be0744266c6a845d18
Drew Miley <drewmiley92@gmail.com>
2019-01-13 02:48:20 +0000

    Adds user by id

--- server.js
+++ server.js
@@ -37,5 +37,39 @@ router.route('/users')
             res.json(users);
         });
     });
+router.route('/users/:id')
+        .get((req, res) => {
+            User.findById(req.params.id, (err, user) => {
+                if (err) {
+                    res.send(err);
+                }
+                res.json(user);
+            });
+        })
+    .put((req, res) => {
+        User.findById(req.params.id, (err, user) => {
+            if (err) {
+                res.send(err);
+            }
+            user.name = req.body.name;
+            user.save(err => {
+                if (err) {
+                    res.send(err);
+                }
+                res.json({ message: 'User updated!' });
+            });
+
+        });
+    })
+    .delete((req, res) => {
+        User.remove({
+            _id: req.params.id
+        }, (err, user) => {
+            if (err) {
+                res.send(err);
+            }
+            res.json({ message: 'User deleted' });
+        });
+    });
 app.use('/api', router);
 app.listen(8080);
